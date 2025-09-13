8ceb66550302b6c81b91b6dfa95060f486e6c695
Kyik <149753097+Kyik@users.noreply.github.com>
2025-09-14 06:04:51 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "student_db";
+define('DB_HOST', "yy-ver1-rds.ctqigw62kpxk.us-east-1.rds.amazonaws.com");
+define('DB_USER', "yen0809");
+define('DB_PASS', "p3TEr100");
+define('DB_NAME', "PETER");
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
