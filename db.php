a437332ee7052b07665e5ab31c15523adc3d2b22
Kyik <149753097+Kyik@users.noreply.github.com>
2025-09-14 06:09:16 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', "xyzuni-db.c3m4my2w227j.us-east-1.rds.amazonaws.com");
-define('DB_USER', "nodeapp");
-define('DB_PASS', "student12");
+define('DB_USER', "admin");
+define('DB_PASS', "cllee0311.");
 define('DB_NAME', "STUDENTS");
 
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
