7d6cc56695fa936669c377c8238ee09ba9a3efef
Kyik <149753097+Kyik@users.noreply.github.com>
2025-09-14 06:16:01 +0800

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ define('DB_USER', "admin");
 define('DB_PASS', "cllee0311.");
 define('DB_NAME', "STUDENTS");
 
-$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
