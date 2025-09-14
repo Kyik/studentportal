cdceb6b942da89dab9e988c37bebe50fbcc1f460
Kyik <149753097+Kyik@users.noreply.github.com>
2025-09-14 15:38:52 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
-
-define('DB_HOST', "xyzuni-db.c3m4my2w227j.us-east-1.rds.amazonaws.com");
-define('DB_USER', "admin");
-define('DB_PASS', "cllee0311.");
-define('DB_NAME', "STUDENTS");
+$host = "studentdb.c3m4my2w227j.us-east-1.rds.amazonaws.com";  // your RDS endpoint
+$user = "admin";   // your RDS username
+$pass = "studentDB"; 
+$dbname = "student_db";
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
